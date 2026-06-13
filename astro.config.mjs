17d0c43e7f2d32e60d7c8c6b8e9025526dc8c87e
Suraj Gusain <suraj.gusain15@gmail.com>
2026-06-13 19:07:24 +0530

    updated astro.config.mjs with correct integrations and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://surajgusain15.github.io",
   base: "/sunstarconsultancy",
-  integrations: [mdx(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [tailwind(), mdx(), sitemap()],
 });
